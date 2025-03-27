670351d916f0a9322d7c1e3695cfbc9aad71f18b
Luis <luisdavidmarte00@gmail.com>
2025-03-26 21:02:47 -0400

    Composer files

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php 
+        require_once __DIR__ . '/../vendor/autoload.php'; // carga las dependencias
+
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+
+        function conectarDB() {
+            return new mysqli(
+                $_ENV['MYSQL_HOST'],
+                $_ENV['MYSQL_USER'],
+                $_ENV['MYSQL_PASSWORD'],
+                $_ENV['MYSQL_PORT']
+            );
+        }
+
+?>
